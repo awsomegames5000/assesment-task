a2a10c59a3e9b23141b0d10408fe7e1ab7639eb7
awsomegames5000 <163372933+awsomegames5000@users.noreply.github.com>
2024-05-14 15:03:57 +1000

    created nested loop functions
    also created a restart function with the button after finishing rather than stopping functionality after test

--- main.py
+++ main.py
@@ -3,33 +3,47 @@ from tkinter import*
 
 current_label = None
 
-def incorrect():
-    global current_label
-    if current_label:
-            current_label.pack_forget()
-    correct_answer = number * number2
-    current_label = Label(root, text=f"Incorrect, the answer is {correct_answer}.")
-    current_label.pack()
-    wid1.delete(0, END)
-    return False
-
-def ans_question():
-    try: 
-        global current_label
-        if current_label:
-            current_label.pack_forget()
-        answer = int(wid1.get())
-        correct_answer = number * number2
-        if answer == correct_answer:
-            current_label = Label(root, text='Correct')
-            current_label.pack()
-            wid1.delete(0, END)
-            return True
-        else:
+def test():
+    def ans_question():
+        def incorrect():
+            global current_label
+            if current_label:
+                current_label.pack_forget()
+        try: 
+            global current_label
+            if current_label:
+                current_label.pack_forget()
+            answer = int(wid1.get())
+            correct_answer = number * number2
+            if answer == correct_answer:
+                current_label = Label(root, text='Correct')
+                current_label.pack()
+                wid1.delete(0, END)
+                return True
+            else:
+                incorrect()
+        except ValueError:
             incorrect()
-    except ValueError:
-        incorrect()
+        correct_answer = number * number2
+        current_label = Label(root, text=f"Incorrect, the answer is {correct_answer}.")
+        current_label.pack()
+        wid1.delete(0, END)
+        return False
     
+    ans_correct = 0
+    for i in range(12):
+        
+        number = random.randint(1, 12)  
+        number2 = random.randint(1, 12)
+        label.config(text=f"What is {number} x {number2}?")
+        root.mainloop()
+        if ans_question():
+            ans_correct += 1
+    result=Label(root, text=f"Test completed: Score is {ans_correct}/12")
+    result.pack()
+    Button1.config(command=test())
+    
+    result.pack_forget()
     
 root=Tk()
 root.title('Question')
@@ -39,7 +53,6 @@ root.resizable(False, False)
 
 wid1=Entry(root, text='enter first number')
 wid1.pack(pady=20)
-
 
 label = Label(root)
 label.pack()
@@ -54,17 +67,8 @@ result_label.pack()
 ans_correct=Label(root, text='')
 ans_correct.pack()
 
-ans_correct = 0
-for i in range(12):
-    number = random.randint(1, 12)  
-    number2 = random.randint(1, 12)
-    label.config(text=f"What is {number} x {number2}?")
-    root.mainloop()
-    if ans_question():
-         ans_correct += 1
-question=Label(root, text=f"Test completed: Score is {ans_correct}/12")
-question.pack()
-Button1.config(command=lambda:None)
+if __name__ == '__main__':
+    test()
 
 root.mainloop()
 
