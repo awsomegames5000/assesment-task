015de2f20351c0a58c0652603d4c1c1986e314ad
awsomegames5000 <163372933+awsomegames5000@users.noreply.github.com>
2024-05-25 20:09:22 +1000

    Created multiple buttons using list
    Using prior knowledge from the usage of lists for our lesson in radio buttons, I utilised this by adding multiple operation  buttons at a time without writing them individually. This will potentially allow me to return an operation after clicking their corresponding button. (i.e. clicking + will return + on the ans_question function.) This will be the next step in producing the program, combining the stub with the main list and the buttons which I have added.

--- main.py
+++ main.py
@@ -7,33 +7,30 @@ root.geometry('500x250')
 
 def main():
     global frame1
-    global btn1
-    global label
+    global buttons
+    global button
 
-    frame1 = Frame(root,width=500,height=250,)
-    frame1.pack()
+    frame1 = Frame(root)
+    frame1.pack(padx=10, pady=10)
 
-    btn1 = Button(frame1,text='Start',command=run)
-    btn1.pack(pady=20)
+    # Button labels
+    button_labels = ['+', '-', '*', '/']
 
-    label=Label(frame1,text='Multiplication Quiz')
-    label.pack(expand=False)
+    # Create the buttons and add them to the frame
+    buttons = []
+    for label in button_labels:
+        button = Button(frame1, text=label, command=lambda label=label: run(label))
+        button.pack(side=LEFT, padx=5)
+        buttons.append(button)
 
-def run():
+def run(operation):
     global framerun
     global frame1
     global result
 
     frame1.pack_forget()
     def incorrect():
-        global current_label
-        if current_label:
-                current_label.pack_forget()
-        correct_answer = number * number2
-        current_label = Label(root, text=f"Incorrect, the answer is {correct_answer}.")
-        current_label.pack()
-        wid1.delete(0, END)
-        return False
+        pass
 
     def ans_question():
         try: 
@@ -99,15 +96,16 @@ def res():
     global frame1
     global MainMenu
     global final_result
+    global buttons
+    global button
 
     MainMenu.pack_forget()
     final_result.pack_forget()
 
     frame1.pack()
 
-    btn1.pack(pady=20)
+    buttons.append(button)
 
-    label.pack(expand=False)
 
 current_label=None
 main()
