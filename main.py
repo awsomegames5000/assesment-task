367ac9bbc5c26319f67fbefb1e1fe7051443f802
awsomegames5000 <163372933+awsomegames5000@users.noreply.github.com>
2024-05-26 13:30:27 +1000

    Created operations quiz corresponding to each button
    Using the parameter that I have provided in each of the button commands, I was able to add an if statement on the parameter which corresponds to the desired operation. The next steps to finishing the program is adding a restart button on top of the main menu and fixing the padding issue which seems to push the  quiz down every time the main menu button is pressed.

--- main.py
+++ main.py
@@ -29,8 +29,15 @@ def run(operation):
     global result
 
     frame1.pack_forget()
+
     def incorrect():
-        pass
+        global current_label
+        if current_label:
+            current_label.pack_forget()
+        current_label = Label(root, text=f'Incorrect. The answer is {correct_answer}')
+        current_label.pack()
+        wid1.delete(0, END)
+        return False
 
     def ans_question():
         try: 
@@ -38,7 +45,6 @@ def run(operation):
             if current_label:
                 current_label.pack_forget()
             answer = int(wid1.get())
-            correct_answer = number * number2
             if answer == correct_answer:
                 current_label = Label(root, text='Correct')
                 current_label.pack()
@@ -66,9 +72,27 @@ def run(operation):
 
     ans_correct = 0
     for i in range(12):
-        number = random.randint(1, 12)  
-        number2 = random.randint(1, 12)
-        label.config(text=f"What is {number} x {number2}?")
+        if operation =='*':
+            number = random.randint(1, 12)  
+            number2 = random.randint(1, 12)
+            correct_answer = number * number2
+            label.config(text=f"What is {number} x {number2}?")
+        elif operation =='+':
+            number = random.randint(1, 100)  
+            number2 = random.randint(1, 100)
+            correct_answer = number + number2
+            label.config(text=f"What is {number} + {number2}?")
+        elif operation =='-':
+            number = random.randint(1, 100)  
+            number2 = random.randint(1, 100)
+            correct_answer = number - number2
+            label.config(text=f"What is {number} - {number2}?")
+        else:
+            number1 = random.randint(1, 12)  
+            number2 = random.randint(1, 12)
+            product = number1 * number2
+            correct_answer=product / number1
+            label.config(text=f"What is {product} / {number1}? ")
         root.mainloop()
         if ans_question():
             ans_correct += 1
@@ -111,3 +135,8 @@ current_label=None
 main()
 
 root.mainloop()
+
+#to do
+#fix padding to which main menu doesn't keep going down
+#add restart function (OPTIONAL)
+#DECORATE!!!!!!!!!!!!
