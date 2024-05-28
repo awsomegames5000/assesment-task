cdf1e934bf548fe130e82a9972b5e1195b7d8650
awsomegames5000 <163372933+awsomegames5000@users.noreply.github.com>
2024-05-28 21:17:07 +1000

    Major design changes, fixed widget ordering
    I fixed the widget ordering of the program during the framerun() container since the question number heading was at the bottom. I reversed the pack order of each widget and used side='bottom' to put the heading at the top. I also added colour to the program as well as enlarging the buttons and text for better readability. The main menu buttons now take up the entire menu.

--- main.py
+++ main.py
@@ -5,25 +5,36 @@ root = Tk()
 
 root.geometry('500x250')
 
-root.resizable(False,False)
+root.resizable(False,False) # makes window of the program static; it cannot change.
+
 
 def main():
-    global frame1
+    global frame1 #each frame corresponds to an interface shown in my storyboard. The frame acts as a master
+                    #which contains the desired widgets I wish to keep and wipe out.
 
-    frame1 = Frame(root,bg='green',width=500,height=250)
+    frame1 = Frame(root,
+                   bg='dark green',
+                   width=500,height=250)
     frame1.pack()
     frame1.pack_propagate(0)
 
-    title = Label(frame1, bg='green',text='Math Operation Quiz!',font=('Arial',24))
+    title = Label(frame1, 
+                  bg='dark green',
+                  text='Math Operation Quiz!',
+                  font=('Arial',24,'bold'))
     title.pack()
 
     signs_labels = ['+', '-', 'x', '÷']
 
-    buttons = []
-
     for signs in signs_labels:
-        button = Button(frame1, text=signs, font=('Arial', 30),command=lambda label=signs: run(label))
-        buttons.append(button)
+
+        button = Button(frame1, 
+                        text=signs, 
+                        font=('Arial', 48,'bold'),
+                        bg='maroon',
+                        fg='white',
+                        activebackground='VioletRed3',
+                        command=lambda label=signs: run(label))
         button.pack(side=LEFT, padx=5, pady=5, expand = True, fill='both')
 
 def run(operation):
@@ -35,8 +46,11 @@ def run(operation):
         global current_label
         if current_label:
             current_label.pack_forget()
-        current_label = Label(root, text=f'Incorrect. The answer is {correct_answer}')
-        current_label.pack()
+        current_label = Label(framerun, 
+                              bg='dark green', 
+                              font=('Arial',14),
+                              text=f'Incorrect. The answer is {correct_answer}')
+        current_label.pack(side='bottom')
         wid1.delete(0, END)
         return False
 
@@ -47,8 +61,11 @@ def run(operation):
                 current_label.pack_forget()
             answer = int(wid1.get())
             if answer == correct_answer:
-                current_label = Label(root, text='Correct')
-                current_label.pack()
+                current_label = Label(framerun,
+                                      bg='dark green', 
+                                      font=('Arial',14),
+                                      text='Correct')
+                current_label.pack(side='bottom')
                 wid1.delete(0, END)
                 return True
             else:
@@ -57,25 +74,41 @@ def run(operation):
             incorrect()
 
     frame1.pack_forget()
-    framerun = Frame(root,width=500,height=250)
+    framerun = Frame(root,width=500,height=250,bg='dark green')
     framerun.pack()
+    framerun.pack_propagate(0)
 
-    wid1=Entry(framerun, text='enter first number')
-    wid1.pack(pady=20)
+    #All widgets in this frame are packed with the side at the bottom to make it appear
+    #that the question number is the heading (qnumber). I had reversed order of the widgets
+    #to make this work.
+    Button1= Button(framerun, 
+                    text='Enter',
+                    font=('Arial',16),
+                    bg='maroon',
+                    activebackground='VioletRed3',
+                    command=root.quit)
+    Button1.pack(side='bottom',expand='true')
 
+    wid1=Entry(framerun, 
+               font=('Arial',18),
+               text='enter first number')
+    wid1.pack(side='bottom',expand=True)
 
-    label = Label(framerun)
-    label.pack(pady=20)
+    label = Label(framerun, 
+                  bg='dark green',
+                  font=('Arial',18))
+    label.pack(side='bottom')
 
-    Button1= Button(framerun, text='The button', command=root.quit)
-    Button1.pack()
 
     ans_correct = 0
     for i in range(12):
         if qnumber:
             qnumber.pack_forget()
-        qnumber=Label(framerun,text=f'question {i+1}')
-        qnumber.pack()
+        qnumber=Label(framerun, 
+                      bg='dark green',
+                      font=('Arial',24,'bold'),
+                      text=f'question {i+1}')
+        qnumber.pack(side='bottom')
         if operation =='x':
             number = random.randint(1, 12)  
             number2 = random.randint(1, 12)
@@ -100,7 +133,9 @@ def run(operation):
         root.mainloop()
         if ans_question():
             ans_correct += 1
+    current_label.pack_forget()
             
+    qnumber.pack()
     Button1.config(text='Finish',command=lambda:finish(ans_correct,operation))
 
 def finish(ans_correct, operation):
@@ -112,13 +147,37 @@ def finish(ans_correct, operation):
         current_label.pack_forget()
     framerun.pack_forget()
 
-    finalframe = Frame(root, width=500, height=250)
-    finalframe.pack(pady=20)
-    final_result = Label(finalframe, text=f"Test completed: Score is {ans_correct}/12")
+    finalframe = Frame(root, 
+                       bg='dark green',
+                       width=500, height=300)
+    finalframe.pack()
+    finalframe.pack_propagate(0)
+
+    final_result = Label(finalframe,
+                         bg='dark green',
+                         font=('Arial',24),
+                        text="Test completed, your Score is:")
     final_result.pack()
-    MainMenu = Button(finalframe, text='Main Menu', command=res)
-    MainMenu.pack()
-    retry = Button(finalframe, text='Try Again', command=lambda:restart(operation))
+
+    score = Label(finalframe,
+                  bg='dark green',
+                  font=('Arial',24, 'bold'),
+                  text=f'{ans_correct}/12')
+    score.pack()
+    MainMenu = Button(finalframe, 
+                      bg='maroon',
+                      activebackground='VioletRed3',
+                      text='Main Menu', 
+                      font=('Arial',14),
+                      command=res)
+    
+    MainMenu.pack(pady=20)
+    retry = Button(finalframe, 
+                   text='Try Again',
+                   font=('Arial',14),
+                   bg='maroon', 
+                   activebackground='VioletRed3',
+                   command=lambda:restart(operation))
     retry.pack()
 
 def restart(operation):
