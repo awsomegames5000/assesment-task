18f552d223f0fab7ee6d8a97f320a77655f30de1
awsomegames5000 <163372933+awsomegames5000@users.noreply.github.com>
2024-05-28 15:16:03 +1000

    Created Question numbers on framerun
    The question number keeps track of which question you are on out of 12. I have also added some colour on the main menu and enlarged the operation buttons.

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ root = Tk()
 
 root.geometry('500x250')
 
+root.resizable(False,False)
+
 def main():
     global frame1
 
-    frame1 = Frame(root)
+    frame1 = Frame(root,bg='green',width=500,height=250)
     frame1.pack()
+    frame1.pack_propagate(0)
 
-    signs_labels = ['+', '-', '*', '/']
+    title = Label(frame1, bg='green',text='Math Operation Quiz!',font=('Arial',24))
+    title.pack()
+
+    signs_labels = ['+', '-', 'x', '÷']
 
     buttons = []
 
     for signs in signs_labels:
-        button = Button(frame1, text=signs, command=lambda label=signs: run(label))
-        button.pack(side=LEFT, padx=5)
+        button = Button(frame1, text=signs, font=('Arial', 30),command=lambda label=signs: run(label))
         buttons.append(button)
+        button.pack(side=LEFT, padx=5, pady=5, expand = True, fill='both')
 
 def run(operation):
     global framerun
     global frame1
-
-    frame1.pack_forget()
+    global qnumber
 
     def incorrect():
         global current_label
@@ -62,13 +67,16 @@ def run(operation):
     label = Label(framerun)
     label.pack(pady=20)
 
-
     Button1= Button(framerun, text='The button', command=root.quit)
     Button1.pack()
 
     ans_correct = 0
     for i in range(12):
-        if operation =='*':
+        if qnumber:
+            qnumber.pack_forget()
+        qnumber=Label(framerun,text=f'question {i+1}')
+        qnumber.pack()
+        if operation =='x':
             number = random.randint(1, 12)  
             number2 = random.randint(1, 12)
             correct_answer = number * number2
@@ -88,10 +96,11 @@ def run(operation):
             number2 = random.randint(1, 12)
             product = number1 * number2
             correct_answer=product // number1
-            label.config(text=f"What is {product} / {number1}? ")
+            label.config(text=f"What is {product} ÷ {number1}? ")
         root.mainloop()
         if ans_question():
             ans_correct += 1
+            
     Button1.config(text='Finish',command=lambda:finish(ans_correct,operation))
 
 def finish(ans_correct, operation):
@@ -128,6 +137,7 @@ def res():
     frame1.pack()
 
 current_label=None
+qnumber = None
 main()
 
 root.mainloop()
